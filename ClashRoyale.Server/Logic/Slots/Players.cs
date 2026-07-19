7ba09cf5d5e6c04678ee23cc165f52674633fe19
Chimera <pixelmcpe1@gmail.com>
2026-07-19 17:56:17 +0800

    fixed players.cs

--- ClashRoyale.Server/Logic/Slots/Players.cs
+++ ClashRoyale.Server/Logic/Slots/Players.cs
@@ -12,10 +12,48 @@ namespace ClashRoyale.Server.Logic.Slots
     internal class Players : ConcurrentDictionary<long, Player>
     {
         internal long Seed;
+        private static readonly object InitLock = new object();
+        private static volatile bool Initialized;
+        private static readonly Random TokenRandom = new Random();
+        private static readonly object TokenRandomLock = new object();
 
         public Players()
         {
+            EnsureDatabaseInitialized();
             Seed = MySQL.GetSeed("Players", "UserID");
+        }
+
+        private static void EnsureDatabaseInitialized()
+        {
+            if (Initialized)
+                return;
+
+            lock (InitLock)
+            {
+                if (Initialized)
+                    return;
+
+                using (var Database = new Context())
+                {
+                    var Warmup = Database.Players.FirstOrDefault();
+                }
+
+                Initialized = true;
+            }
+        }
+
+        private static string GenerateToken(int length = 40)
+        {
+            const string Chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+            var Buffer = new char[length];
+
+            lock (TokenRandomLock)
+            {
+                for (var i = 0; i < length; i++)
+                    Buffer[i] = Chars[TokenRandom.Next(Chars.Length)];
+            }
+
+            return new string(Buffer);
         }
 
         internal void Add(Player Player)
@@ -38,7 +76,6 @@ namespace ClashRoyale.Server.Logic.Slots
         internal void Remove(Player Player)
         {
             Player TmpPlayer;
-
             if (ContainsKey(Player.UserID))
                 if (!TryRemove(Player.UserID, out TmpPlayer))
                 {
@@ -50,39 +87,27 @@ namespace ClashRoyale.Server.Logic.Slots
             if (!ContainsKey(UserID))
             {
                 Player Player = null;
-
                 using (var Database = new Context())
                 {
                     var Data = Database.Players.Find(UserID);
-
                     if (Data != null)
                         if (!string.IsNullOrEmpty(Data.Data))
                         {
                             Player = new Player(null, UserID);
-
                             JsonConvert.PopulateObject(Data.Data, Player);
-
                             if (Store)
                                 Add(Player);
                         }
                 }
-
                 return Player;
             }
-
             return this[UserID];
         }
 
         internal Player CreatePlayer(Device Device, long UserID = 0, bool Store = true)
         {
             var Player = new Player(Device, Interlocked.Increment(ref Seed));
-
-            var Chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-
-            var RandomToken = Enumerable.Repeat(Chars, 40)
-                .Select(s => s[new Random().Next(s.Length)]).ToArray();
-
-            foreach (var Letter in RandomToken) Player.Token += Letter;
+            Player.Token = GenerateToken();
 
             using (var Database = new Context())
             {
@@ -91,10 +116,8 @@ namespace ClashRoyale.Server.Logic.Slots
                     UserID = Player.UserID,
                     Data = JsonConvert.SerializeObject(Player, Formatting.Indented)
                 };
-
                 Database.Players.Add(PlayerModel);
                 Database.SaveChanges();
-
                 if (Store)
                     Add(Player);
             }
@@ -107,21 +130,17 @@ namespace ClashRoyale.Server.Logic.Slots
             using (Context Database = new Context())
             {
                 PlayerModel Data = Database.Players.Find(Player.UserID);
-
                 if (Data != null)
                 {
-                    Data.UserID = Player.UserID;
                     Data.Data = JsonConvert.SerializeObject(Player, Formatting.Indented);
+                    Database.SaveChanges();
                 }
-
-                Database.SaveChangesAsync();
             }
         }
 
         internal void SaveAll()
         {
             Player[] Players = this.Values.ToArray();
-
             Parallel.ForEach(Players, Player =>
             {
                 try
